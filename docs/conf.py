dc96a44723a7b5fe914895f2e0ef2531f3cbfb6c
jbusecke <julius@ldeo.columbia.edu>
2018-03-15 20:54:28 -0400

    bootstrap theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinx_bootstrap_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -78,7 +79,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

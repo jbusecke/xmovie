2a300f139100659d759675f1a1dfb67db3c7041f
jbusecke <julius@ldeo.columbia.edu>
2020-01-06 18:55:58 -0500

    Fixed test to empty kwargs

--- xmovie/test/test_core.py
+++ xmovie/test/test_core.py
@@ -259,7 +259,7 @@ def test_Movie(plotfunc, framedim, frame_pattern, dpi, pixelheight, pixelwidth):
 
     # this should work (this way one could pass a totally custom function)
     mov = Movie(ds, input_check=False)
-    assert mov.kwargs == kwargs #this still needs to be set
+    assert mov.kwargs == {} #there are no kwargs set that are not used by Movie
 
 
 @pytest.mark.parametrize(

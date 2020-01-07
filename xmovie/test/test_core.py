f088f0eec92eac3eec51a627afdf2f183f58099b
Julius Busecke <jbusecke@princeton.edu>
2020-01-06 19:52:54 -0500

    Bug kwargs wo inputcheck (#23)
    * Set movie.kwargs even if input_check is deactivated. Enhanced test
    
    * Set movie.kwargs even if input_check is deactivated. Enhanced test
    
    * Fix merge conflict
    
    * Fixed test to empty kwargs

--- xmovie/test/test_core.py
+++ xmovie/test/test_core.py
@@ -259,6 +259,7 @@ def test_Movie(plotfunc, framedim, frame_pattern, dpi, pixelheight, pixelwidth):
 
     # this should work (this way one could pass a totally custom function)
     mov = Movie(ds, input_check=False)
+    assert mov.kwargs == {} #there are no kwargs set that are not used by Movie
 
 
 @pytest.mark.parametrize(

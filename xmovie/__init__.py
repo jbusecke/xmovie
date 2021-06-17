313cba864ba806e83d7aa93c463d8c8957942976
zmoon <zmoon92@gmail.com>
2021-06-17 14:33:04 -0400

    Improve __init__

--- xmovie/__init__.py
+++ xmovie/__init__.py
@@ -1,3 +1,12 @@
-__version__ = "0.1.0"
 from .core import Movie
 from .presets import rotating_globe
+
+
+try:
+    from ._version import __version__
+
+except ModuleNotFoundError:
+    __version__ == "999"
+
+
+__all__ = ("Movie", "rotating_globe")
